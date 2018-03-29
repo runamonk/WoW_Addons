923a8ed79435eadcdc4e1bed99f7c629cbc79213
runamonk <runamonk@gmail.com>
2018-03-29 21:12:12 +0100

    License.

--- mnkUnits/mnkUnits.lua
+++ mnkUnits/mnkUnits.lua
@@ -60,7 +60,6 @@ local function PostCreateIcon(Auras, button)
     local timer = button.cd:GetRegions()
     timer:SetFont(mnkLibs.Fonts.ap, 10, 'OUTLINE')
     timer:SetPoint('BOTTOMLEFT', button, 0, 0)
-
     button.icon:SetTexCoord(.07, .93, .07, .93)
     button.overlay:SetTexture(mnkLibs.Textures.border)
     button.overlay:SetTexCoord(0, 1, 0, 1)
