f5d4ed0d33785938fd2b30027f3d35ae13657d60
runamonk <runamonk@gmail.com>
2018-08-09 14:45:32 +0100

    Added class colored border.

--- mnkMinimap/mnkMinimap.lua
+++ mnkMinimap/mnkMinimap.lua
@@ -1,6 +1,10 @@
 -- Code based on Zorks rMinimap and rObjectiveTracker.
 
 mnkMinimap = CreateFrame('Frame')
+local _, playerClass = UnitClass('player')
+local classColor = {}
+classColor.r, classColor.g, classColor.b, _ = GetClassColor(playerClass)
+
 
 local function Zoom(self, direction)
   if (direction > 0) then 
@@ -20,14 +24,15 @@ end
 function mnkMinimap:SetMinimapPositionAndSize()
   MinimapCluster:SetScale(1)
   MinimapCluster:ClearAllPoints()
-  MinimapCluster:SetPoint("CENTER", UIParent, "BOTTOM", 0, 70)
+  MinimapCluster:SetPoint('CENTER', UIParent, 'BOTTOM', 0, 70)
   MinimapCluster:SetSize(135,135) 
   
   Minimap:SetMaskTexture(mnkLibs.Textures.minimap_mask)
   Minimap:ClearAllPoints()
-  Minimap:SetPoint("CENTER", UIParent, "BOTTOM", 0, 70)
+  Minimap:SetPoint('CENTER', UIParent, 'BOTTOM', 0, 70)
   Minimap:SetSize(135,135) 
   Minimap:EnableMouseWheel()
+  mnkLibs.createBorder(Minimap, 1, -3, -3, 3, {classColor.r, classColor.g, classColor.b, .5}) 
 
   MinimapBackdrop:Hide()
   MinimapBorder:Hide()
@@ -38,38 +43,38 @@ function mnkMinimap:SetMinimapPositionAndSize()
   MinimapZoneText:Hide()
 
   MiniMapInstanceDifficulty:ClearAllPoints()
-  MiniMapInstanceDifficulty:SetPoint("BOTTOMRIGHT",Minimap,"BOTTOMLEFT", -4, 0)
+  MiniMapInstanceDifficulty:SetPoint('BOTTOMRIGHT', Minimap, 'BOTTOMLEFT', -4, 0)
   MiniMapInstanceDifficulty:SetScale(0.7)
   GuildInstanceDifficulty:ClearAllPoints()
-  GuildInstanceDifficulty:SetPoint("BOTTOMRIGHT",Minimap,"BOTTOMLEFT",-4,-5)
+  GuildInstanceDifficulty:SetPoint('BOTTOMRIGHT', Minimap, 'BOTTOMLEFT',-4,-5)
   GuildInstanceDifficulty:SetScale(0.7)
   MiniMapChallengeMode:ClearAllPoints()
-  MiniMapChallengeMode:SetPoint("BOTTOMRIGHT",Minimap,"BOTTOMLEFT",-4,-10)
+  MiniMapChallengeMode:SetPoint('BOTTOMRIGHT', Minimap, 'BOTTOMLEFT',-4,-10)
   MiniMapChallengeMode:SetScale(0.7)
   
   QueueStatusMinimapButton:SetParent(Minimap)
   QueueStatusMinimapButton:SetScale(1)
   QueueStatusMinimapButton:ClearAllPoints()
-  QueueStatusMinimapButton:SetPoint("TOPLEFT", Minimap, -30, -15)
+  QueueStatusMinimapButton:SetPoint('TOPLEFT', Minimap, -30, -15)
   QueueStatusMinimapButtonBorder:Hide()
   QueueStatusMinimapButton:SetHighlightTexture (nil)
   QueueStatusMinimapButton:SetPushedTexture(nil)
   
   MiniMapMailFrame:ClearAllPoints()
-  MiniMapMailFrame:SetPoint("BOTTOMRIGHT",Minimap, 30, -5)
+  MiniMapMailFrame:SetPoint('BOTTOMRIGHT', Minimap, 30, -5)
   MiniMapMailIcon:SetTexture(mnkLibs.Textures.minimap_mail)
   MiniMapMailBorder:SetAlpha(0)
   
   MiniMapTracking:SetParent(Minimap)
   MiniMapTracking:SetScale(1)
   MiniMapTracking:ClearAllPoints()
-  MiniMapTracking:SetPoint("TOPLEFT", Minimap, -30, 5)
+  MiniMapTracking:SetPoint('TOPLEFT', Minimap, -30, 5)
   MiniMapTrackingButton:SetHighlightTexture (nil)
   MiniMapTrackingButton:SetPushedTexture(nil)
   MiniMapTrackingBackground:Hide()
   MiniMapTrackingButtonBorder:Hide()
  
-  LoadAddOn("Blizzard_TimeManager")
+  LoadAddOn('Blizzard_TimeManager')
   TimeManagerClockButton:GetRegions():Hide()
   TimeManagerClockButton:ClearAllPoints()
   TimeManagerClockButton:SetAlpha(0)
@@ -77,7 +82,7 @@ function mnkMinimap:SetMinimapPositionAndSize()
   GameTimeFrame:SetParent(Minimap)
   GameTimeFrame:SetScale(0.5)
   GameTimeFrame:ClearAllPoints()
-  GameTimeFrame:SetPoint("TOPRIGHT", Minimap, 50, 3)
+  GameTimeFrame:SetPoint('TOPRIGHT', Minimap, 50, 3)
   GameTimeFrame:SetHitRectInsets(0, 0, 0, 0)
   GameTimeFrame:GetNormalTexture():SetTexCoord(0,1,0,1)
   GameTimeFrame:SetNormalTexture(mnkLibs.Textures.minimap_calendar)
@@ -86,16 +91,16 @@ function mnkMinimap:SetMinimapPositionAndSize()
 
   local fs = GameTimeFrame:GetFontString()
   fs:ClearAllPoints()
-  fs:SetPoint("CENTER",0,-5)
+  fs:SetPoint('CENTER',0,-5)
   fs:SetFont(STANDARD_TEXT_FONT,20)
   fs:SetTextColor(0.2,0.2,0.1,0.9)
 
-  Minimap:SetScript("OnMouseWheel", Zoom)
+  Minimap:SetScript('OnMouseWheel', Zoom)
 end
 
 function mnkMinimap:SetQuestTrackerPosition()
   ObjectiveTrackerFrame:ClearAllPoints(); 
-  ObjectiveTrackerFrame:SetPoint("TOPRIGHT", UIParent, -5, -25);    
+  ObjectiveTrackerFrame:SetPoint('TOPRIGHT', UIParent, -5, -25);    
   ObjectiveTrackerFrame.SetPoint = mnkLibs.donothing()
   ObjectiveTrackerFrame:SetClampedToScreen(true)
   ObjectiveTrackerFrame:SetMovable(true)
@@ -103,4 +108,4 @@ function mnkMinimap:SetQuestTrackerPosition()
 end
 
 mnkMinimap:SetScript('OnEvent', mnkMinimap.DoOnEvent)
-mnkMinimap:RegisterEvent("PLAYER_ENTERING_WORLD")
+mnkMinimap:RegisterEvent('PLAYER_ENTERING_WORLD')
