dd94b4744dee8e23fb7f4278732775c1110b01ed
runamonk <runamonk@gmail.com>
2018-08-20 14:29:08 +0100

    Changed icon size.

--- mnkProfessions/mnkProfessions.lua
+++ mnkProfessions/mnkProfessions.lua
@@ -78,9 +78,9 @@ function mnkProfessions.GetProfText(p)
     if p ~= nil then
         local _, icon, skillLevel, maxSkillLevel, _, _, _, _, _, _ = GetProfessionInfo(p)
         if skillLevel == maxSkillLevel then
-            return string.format('|T%s:20|t', icon) ..' '..mnkLibs.Color(COLOR_GOLD)..maxSkillLevel
+            return string.format('|T%s:16|t', icon) ..' '..mnkLibs.Color(COLOR_GOLD)..maxSkillLevel
         else 
-            return string.format('|T%s:20|t', icon) ..' '..mnkLibs.Color(COLOR_GOLD)..skillLevel..'/'..maxSkillLevel
+            return string.format('|T%s:16|t', icon) ..' '..mnkLibs.Color(COLOR_GOLD)..skillLevel..'/'..maxSkillLevel
         end
     else
         return ''
