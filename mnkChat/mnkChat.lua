073bac36d8147edb44a7eb2df58118c767da72d9
runamonk <runamonk@gmail.com>
2018-08-23 17:30:46 +0100

    Rewrite.

--- mnkChat/mnkChat.lua
+++ mnkChat/mnkChat.lua
@@ -1,22 +1,8 @@
 mnkChat = CreateFrame("Frame")
 mnkChat.LDB = LibStub:GetLibrary('LibDataBroker-1.1')
-
 local libQTip = LibStub('LibQTip-1.0')
-
-mnkChat_db = {}
-mnkChat_db.Messages = {}
-mnkChat_db.NEW_MESSAGES = 0
-
-local MAX_MESSAGES = 10
-
-local font = CreateFont("tooltipFont")
-font:SetFont(mnkLibs.Fonts.abf, 12)
-
 local hooks = {}
-local foundurl = false
-local hideFrame = function (self) self:Hide() end
-local tabs = {"Left","Middle","Right","SelectedLeft","SelectedRight","SelectedMiddle","HighlightLeft","HighlightMiddle","HighlightRight"}
-SLASH_CLEAR_CHAT1 = "/clear"
+local frames = {}
 
 local StickyTypeChannels = {
     SAY = 1, 
@@ -30,116 +16,58 @@ local StickyTypeChannels = {
     CHANNEL = 1, 
 }
 
+local tabs = {"Left","Middle","Right","SelectedLeft","SelectedRight","SelectedMiddle","HighlightLeft","HighlightMiddle","HighlightRight"}
+
+hooks.ChatFrame_OnHyperlinkShow = ChatFrame_OnHyperlinkShow
+
+SLASH_CLEAR_CHAT1 = "/clear"
 ChatFontNormal:SetFont(mnkLibs.Fonts.ap, 14, '')
 ChatFontNormal:SetShadowOffset(1,1)
 ChatFontNormal:SetShadowColor(0,0,0)
 
-function mnkChat.DoOnChat(event, message, playername, _, _, _, playerstatus, _, _, _, lineid, _, guid, pid)
-    if message ~= nil then
-        CombatText_AddMessage(mnkLibs.formatPlayerName(playername)..': '..message, CombatText_StandardScroll, 255, 0, 0, nil, false)
-        table.insert(mnkChat_db.Messages, 1, {time, name, message})
-        mnkChat_db.Messages[1].time = date('%I:%M:%S:%p')
-        if string.find(playername, '-') == nil then
-            mnkChat_db.Messages[1].name = playername
+FloatingChatFrame_OnMouseScroll = function(self, dir)
+    if(dir > 0) then
+        if(IsShiftKeyDown()) then
+            self:ScrollToTop()
         else
-            mnkChat_db.Messages[1].name = mnkLibs.formatPlayerName(playername)
+            self:ScrollUp()
         end
-        mnkChat_db.Messages[1].fullname = playername
-        mnkChat_db.Messages[1].message = message
-
-        mnkChat_db.NEW_MESSAGES = mnkChat_db.NEW_MESSAGES + 1
-        if #mnkChat > MAX_MESSAGES then
-            for i = #mnkChat_db.Messages, MAX_MESSAGES + 1, -1 do
-                table.remove(mnkChat_db.Messages, i)
-            end
-        end
-    end
-end
-
-function mnkChat.DoOnClick(self, button)
-    if self.tooltip ~= nil then
-        self.tooltip:Hide()
-    end
-
-    if button == 'RightButton' then
-        mnkChat_db.Messages = {}
-        mnkChat.UpdateText()
-    end
-end
-
-function mnkChat.DoOnEnter(self)
-    mnkChat_db.NEW_MESSAGES = 0
-    if #mnkChat_db.Messages == 0 then
-        return
-    end
-
-    mnkChat.UpdateText()
-    local tooltip = libQTip:Acquire('mnkChatTooltip', 2, 'LEFT', 'LEFT')
-    tooltip:SetFont(font)
-
-    self.tooltip = tooltip 
-    tooltip:Clear()
-
-    tooltip:AddHeader(mnkLibs.Color(COLOR_GOLD)..'Name', mnkLibs.Color(COLOR_GOLD)..'Message')
-
-    for i = 1, #mnkChat_db.Messages do
-        if mnkChat_db.Messages[i].message ~= nil then
-            local y, x = tooltip:AddLine(mnkChat_db.Messages[i].name, '')
-            tooltip:SetCell(y, 2, mnkChat_db.Messages[i].time..' '..mnkChat_db.Messages[i].message, nil, 'LEFT', nil, nil, nil, nil, GetScreenWidth() / 4, nil)
-            tooltip:SetLineScript(y, 'OnMouseDown', mnkChat.DoOnMessageClick, mnkChat_db.Messages[i].fullname)
-        end
-    end
-
-    tooltip:SetAutoHideDelay(.1, self)
-    tooltip:SmartAnchorTo(self)
-    tooltip:UpdateScrolling(500)
-    tooltip:SetBackdropBorderColor(0, 0, 0, 0)
-    tooltip:Show()
-end
-
-function mnkChat.DoOnMessageClick(self, arg, button) 
-    SetItemRef('player:'..arg, '|Hplayer:'..arg..'|h['..arg..'|h', 'LeftButton')
-end
-
-function mnkChat.UpdateText()
-    if mnkChat_db.NEW_MESSAGES > 0 then
-        mnkChat.LDB.icon = mnkLibs.Textures.icon_new
     else
-        mnkChat.LDB.icon = mnkLibs.Textures.icon_none
+        if(IsShiftKeyDown()) then
+            self:ScrollToBottom()
+        else
+            self:ScrollDown()
+        end
     end
-    mnkChat.LDB.text = mnkChat_db.NEW_MESSAGES
+end
+SlashCmdList.CLEAR_CHAT = function()
+    for i = 1, NUM_CHAT_WINDOWS do
+        _G[format("ChatFrame%d", i)]:Clear()
+    end
 end
 
+function ChatFrame_OnHyperlinkShow(frame, link, text, button)
+    local type, value = link:match("(%a+):(.+)")
+    if (type == "url") then
+        local eb = _G[frame:GetName()..'EditBox']
+        
+        eb:Show()
+        eb:SetText(value)
+        eb:SetFocus()
+        eb:HighlightText()
+    else
+        hooks.ChatFrame_OnHyperlinkShow(self, link, text, button)
+    end
+end
 function mnkChat:DoOnEvent(event, ...)
     if event == "ADDON_LOADED" then
-        mnkChat:UnregisterEvent('ADDON_LOADED')
-    end
-
-    QuickJoinToastButton:SetScript("OnShow", hideFrame)
-    QuickJoinToastButton:Hide() 
-    --FriendsMicroButton:SetScript("OnShow", hideFrame)
-    ChatFrameMenuButton:SetScript("OnShow", hideFrame) 
-    --FriendsMicroButton:Hide()
-    ChatFrameMenuButton:Hide()
-
-    ChatFrame1ButtonFrame:Hide()
-    ChatFrame1ButtonFrame:SetScript("OnShow", hideFrame) 
-    ChatFrameChannelButton:Hide()
-    ChatFrameChannelButton:SetScript("OnShow", hideFrame) 
-
-    for i = 1, NUM_CHAT_WINDOWS do mnkChat.SetFrameSettings(_G["ChatFrame"..i]) end
-
-    if string.sub(event, 1, 8) == 'CHAT_MSG' then
-        PlaySoundFile(mnkLibs.Sounds.incoming_message, "Master")
-        mnkChat.DoOnChat(event, ...)
-    elseif event == "PLAYER_LOGIN" then
-        mnkChat.LDB = LibStub('LibDataBroker-1.1'):NewDataObject('mnkChat', {
-            icon = mnkLibs.Textures.icon_none, 
-            type = 'data source', 
-            OnEnter = mnkChat.DoOnEnter, 
-            OnClick = mnkChat.DoOnClick
-        }) 
-
+        QuickJoinToastButton:Hide() 
+        QuickJoinToastButton:HookScript("OnShow", QuickJoinToastButton.Hide)
+        ChatFrameMenuButton:Hide()
+        ChatFrameMenuButton:HookScript("OnShow", ChatFrameMenuButton.Hide)
+        ChatFrameChannelButton:Hide()
+        ChatFrameChannelButton:HookScript("OnShow", ChatFrameChannelButton.Hide)
+        
         CHAT_WHISPER_GET = ">> %s: "
         CHAT_WHISPER_INFORM_GET = "<< %s: "
         CHAT_YELL_GET = "|Hchannel:Yell|h<Y> |h %s: "
@@ -154,7 +82,7 @@ function mnkChat:DoOnEvent(event, ...)
         CHAT_RAID_GET = "|Hchannel:Raid|h<R> |h %s: "
         CHAT_RAID_LEADER_GET = [[|Hchannel:Raid|h<RL> |h %s: ]]
         CHAT_RAID_WARNING_GET = [[|Hchannel:RaidWarning|h<RW> |h %s: ]]
-
+        
         CHAT_MONSTER_PARTY_GET = CHAT_PARTY_GET
         CHAT_MONSTER_SAY_GET = CHAT_SAY_GET
         CHAT_MONSTER_WHISPER_GET = CHAT_WHISPER_GET
@@ -163,7 +91,7 @@ function mnkChat:DoOnEvent(event, ...)
         for k, v in pairs(StickyTypeChannels) do
             ChatTypeInfo[k].sticky = v
         end
-
+        
         --toggle class colors
         for i, v in pairs(CHAT_CONFIG_CHAT_LEFT) do
             ToggleChatColorNamesByClassGroup(true, v.type)
@@ -173,40 +101,54 @@ function mnkChat:DoOnEvent(event, ...)
         for iCh = 1, 15 do
             ToggleChatColorNamesByClassGroup(true, "CHANNEL"..iCh)
         end
+
+        for i = 1, NUM_CHAT_WINDOWS do
+            mnkChat.SetupFrame(_G["ChatFrame" .. i])
+        end
+
+    	hooks.FCF_OpenTemporaryWindow = FCF_OpenTemporaryWindow
+        FCF_OpenTemporaryWindow = function(chatType, ...)
+            local frame = hooks.FCF_OpenTemporaryWindow(chatType, ...)
+            mnkChat.SetupFrame(frame)
+            return frame
+        end
+	    self:UnregisterEvent("ADDON_LOADED")
     end
-
-    mnkChat.UpdateText()
 end
 
-local function OnHyperlinkEnter(frame, link, ...)
-    local t = link:match("^([^:]+)")
-    if t == "item" then
-        GameTooltip:SetOwner(frame, "ANCHOR_CURSOR") 
-        GameTooltip:SetHyperlink(link) 
-        GameTooltip:Show() 
-    end
+function mnkChat.AddMessage(frame, message, ...)
+    --stolen straight from rChat by Zork.
+    --channel replace (Trade and such)
+    message = message:gsub('|h%[(%d+)%. .-%]|h', '|h%1.|h')
+    --url search
+    message = message:gsub('([wWhH][wWtT][wWtT][%.pP]%S+[^%p%s])', '|cffffffff|Hurl:%1|h[%1]|h|r')
+
+	hooks[frame].AddMessage(frame, message, ...)
 end
 
-local function OnHyperlinkLeave(frame, ...)
-    GameTooltip:Hide() 
-end
-
-function mnkChat.SetFrameSettings(frame)
+function mnkChat.SetupFrame(frame)
     frame:SetClampRectInsets(0, 0, 0, 0)
     frame:SetMaxResize(UIParent:GetWidth(), UIParent:GetHeight())
     frame:SetMinResize(200, 40) 
     frame:SetFading(false) 
-    frame:SetScript("OnHyperlinkEnter", OnHyperlinkEnter)
-    frame:SetScript("OnHyperlinkLeave", OnHyperlinkLeave)
+    frame:SetScript("OnHyperlinkEnter", function (frame, link, ...)
+        local t = link:match("^([^:]+)")
+        if t == "item" then
+            GameTooltip:SetOwner(frame, "ANCHOR_CURSOR") 
+            GameTooltip:SetHyperlink(link) 
+            GameTooltip:Show() 
+        end
+    end)
+    frame:SetScript("OnHyperlinkLeave", function (frame, ...) GameTooltip:Hide() end)
     frame.ScrollToBottomButton:Hide()
-    frame.ScrollToBottomButton:SetScript("OnShow", hideFrame)
+    frame.ScrollToBottomButton:HookScript("OnShow", frame.ScrollToBottomButton.Hide)
     frame.ScrollBar:Hide()
-    frame.ScrollBar:SetScript("OnShow", hideFrame)
+    frame.ScrollBar:HookScript("OnShow", frame.ScrollBar.Hide)
 
     _G[frame:GetName().."ButtonFrame"]:Hide()
-    _G[frame:GetName().."ButtonFrame"]:SetScript("OnShow", hideFrame)
+    _G[frame:GetName().."ButtonFrame"]:HookScript("OnShow", _G[frame:GetName().."ButtonFrame"].Hide)
     _G[frame:GetName().."ButtonFrameMinimizeButton"]:Hide()
-    _G[frame:GetName().."ButtonFrameMinimizeButton"]:SetScript("OnShow", hideFrame)
+    _G[frame:GetName().."ButtonFrameMinimizeButton"]:HookScript("OnShow", _G[frame:GetName().."ButtonFrameMinimizeButton"].Hide)
     _G[frame:GetName().."EditBox"]:SetAltArrowKeyMode(false)
 
     if GetCVar("chatStyle") == "classic" then
@@ -235,129 +177,22 @@ function mnkChat.SetFrameSettings(frame)
     _G[frame:GetName().."TabText"]:SetVertexColor(1,1,1)
     _G[frame:GetName().."TabText"].SetTextColor = mnkLibs.donothing
     _G[frame:GetName().."TabText"].SetVertexColor = mnkLibs.donothing
-    
+
     -- remove the tab textures.
     for index, value in pairs(tabs) do _G[frame:GetName()..'Tab'..value]:SetTexture(nil) end
+
+	if frame ~= COMBATLOG then
+        if not hooks[frame] then
+            hooks[frame] = {}
+		end
+		if not hooks[frame].AddMessage then
+			hooks[frame].AddMessage = frame.AddMessage
+			frame.AddMessage = mnkChat.AddMessage
+		end
+	end
 end
 
-FloatingChatFrame_OnMouseScroll = function(self, dir)
-    if(dir > 0) then
-        if(IsShiftKeyDown()) then
-            self:ScrollToTop()
-        else
-            self:ScrollUp()
-        end
-    else
-        if(IsShiftKeyDown()) then
-            self:ScrollToBottom()
-        else
-            self:ScrollDown()
-        end
-    end
-end
-
-function string.link(text, type, value)
-    return "|H"..type..":"..tostring(value).."|h"..tostring(text).."|h"
-end
-
-local function highlighturl(before, url, after)
-    foundurl = true
-    return " "..string.link(mnkLibs.Color(COLOR_BLUE).."["..url.."]|r", "url", url).." "
-end
-
-local function searchforurl(frame, text, ...)
-    foundurl = false
-
-    if string.find(text, "%pTInterface%p+") or string.find(text, "%pTINTERFACE%p+") then
-        foundurl = true
-    end
-
-    if not foundurl then
-        --192.168.1.1:1234
-        text = string.gsub(text, "(%s?)(%d%d?%d?%.%d%d?%d?%.%d%d?%d?%.%d%d?%d?:%d%d?%d?%d?%d?)(%s?)", highlighturl)
-    end
-    if not foundurl then
-        --192.168.1.1
-        text = string.gsub(text, "(%s?)(%d%d?%d?%.%d%d?%d?%.%d%d?%d?%.%d%d?%d?)(%s?)", highlighturl)
-    end
-    if not foundurl then
-        --www.teamspeak.com:3333
-        text = string.gsub(text, "(%s?)([%w_-]+%.?[%w_-]+%.[%w_-]+:%d%d%d?%d?%d?)(%s?)", highlighturl)
-    end
-    if not foundurl then
-        --http://www.google.com
-        text = string.gsub(text, "(%s?)(%a+://[%w_/%.%?%%=~&-'%-]+)(%s?)", highlighturl)
-    end
-    if not foundurl then
-        --www.google.com
-        text = string.gsub(text, "(%s?)(www%.[%w_/%.%?%%=~&-'%-]+)(%s?)", highlighturl)
-    end
-    if not foundurl then
-        --lol@lol.com
-        text = string.gsub(text, "(%s?)([_%w-%.~-]+@[_%w-]+%.[_%w-%.]+)(%s?)", highlighturl)
-    end
-
-    text = string.gsub(text, "%[(%d)%. General%]", "<1> ")
-    text = string.gsub(text, "%[(%d)%. Trade%]", "<2> ")
-    text = string.gsub(text, "%[(%d)%. LocalDefense%]", "<3> ")
-
-    local fullName, shortName = strmatch(text, "|Hplayer:(.-)|h%[(.-)%]|h")
-
-    if fullName ~= nil then
-        if strmatch(shortName, "|cff") then
-            shortName = gsub(shortName, "%-[^|]+", "")
-        else
-            shortName = strmatch(shortName, "[^%-]+")
-        end
-        text = gsub(text, "|Hplayer:(.-)|h%[(.-)%]|h", format("|Hplayer:%s|h[%s]|h", fullName, shortName))
-    end
-    --print(fullName, " ", shortName)
-    frame.am(frame, text, ...)
-end
-
-for i = 1, NUM_CHAT_WINDOWS do
-    if (i ~= 2) then
-        local cf = _G["ChatFrame"..i]
-        cf.am = cf.AddMessage
-        cf.AddMessage = searchforurl
-    end
-end
-
--- Stolen straight from PhanxChat.
-hooks.FCF_OpenTemporaryWindow = FCF_OpenTemporaryWindow
-FCF_OpenTemporaryWindow = function(chatType, ...)
-    local frame = hooks.FCF_OpenTemporaryWindow(chatType, ...)
-
-    frame.am = frame.AddMessage
-    frame.AddMessage = searchforurl
-
-    mnkChat.SetFrameSettings(frame)
-    return frame
-end
-
-local orig = ChatFrame_OnHyperlinkShow
-function ChatFrame_OnHyperlinkShow(frame, link, text, button)
-    local type, value = link:match("(%a+):(.+)")
-    if (type == "url") then
-        local eb = _G[frame:GetName()..'EditBox']
-        
-        eb:Show()
-        eb:SetText(value)
-        eb:SetFocus()
-        eb:HighlightText()
-    else
-        orig(self, link, text, button)
-    end
-end
-
-SlashCmdList.CLEAR_CHAT = function()
-    for i = 1, NUM_CHAT_WINDOWS do
-        _G[format("ChatFrame%d", i)]:Clear()
-    end
-end
 
 mnkChat:SetScript("OnEvent", mnkChat.DoOnEvent)
-mnkChat:RegisterEvent("PLAYER_LOGIN")
 mnkChat:RegisterEvent("ADDON_LOADED")
-mnkChat:RegisterEvent("CHAT_MSG_WHISPER")
-mnkChat:RegisterEvent('CHAT_MSG_BN_WHISPER')
+
