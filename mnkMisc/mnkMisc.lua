acaa340da61c993a8126767f3941f522f416f1c6
runamonk <runamonk@gmail.com>
2018-08-24 14:04:09 +0100

    Cleanup

--- mnkMisc/mnkMisc.lua
+++ mnkMisc/mnkMisc.lua
@@ -1,31 +1,21 @@
 mnkMisc = CreateFrame('Frame')
+local myaddonName = ...
 
-
-function mnkMisc:DoOnEvent(event, arg1, arg2)
-    mnkMisc.HideOrderCommandBar()
-
+function mnkMisc:DoOnEvent(event, ...)
     if event == 'ADDON_LOADED' then
-        local b = _G.OrderHallCommandBar
-        if b then
-            --print('OrderHallCommandBar Hidden.')
-            b:HookScript("OnShow", function() mnkMisc.HideOrderCommandBar() end)
-            b:Hide()
+        local addonName = ...
+        if addonName == 'Blizzard_TalkingHeadUI' then
+            hooksecurefunc('TalkingHeadFrame_PlayCurrent', function()
+                TalkingHeadFrame_CloseImmediately()
+                TalkingHeadFrame:Hide()
+            end)
+        elseif addonName == myaddonName then
+            SetCVar('alwaysCompareItems', 0)
+            SetCVar('autoLootDefault', 1)
+            SetCVar('enableFloatingCombatText', 1)
+            OrderHall_CheckCommandBar = mnkLibs.donothing
         end
-
-        end    
-        if arg1 == 'Blizzard_TalkingHeadUI' then
-        hooksecurefunc('TalkingHeadFrame_PlayCurrent', function()
-            TalkingHeadFrame_CloseImmediately()
-            TalkingHeadFrame:Hide()
-        end)
-    end
-
-    if event == 'PLAYER_ENTERING_WORLD' then
-        SetCVar('alwaysCompareItems', 0)
-        SetCVar('autoLootDefault', 1)
-        SetCVar('enableFloatingCombatText', 1)
-    end        
-
+    end    
 end
 
 SLASH_RL1 = '/rl'
@@ -33,22 +23,13 @@ function SlashCmdList.RL(msg, editbox)
     ReloadUI()
 end
 
-function mnkMisc.HideOrderCommandBar()
-    local b = _G.OrderHallCommandBar
-	if b then
-        b:Hide()
-    end
-end
-
 -- Hide dragon heads
 MainMenuBarArtFrame.LeftEndCap:Hide()
 MainMenuBarArtFrame.RightEndCap:Hide()
-
 
 --Hide open all mail button, it's buggy.
 OpenAllMail:Hide()
 
 mnkMisc:SetScript('OnEvent', mnkMisc.DoOnEvent)
 mnkMisc:RegisterEvent('ADDON_LOADED')
-mnkMisc:RegisterEvent('PLAYER_ENTERING_WORLD')
 
