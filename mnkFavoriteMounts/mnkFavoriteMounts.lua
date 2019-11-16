faf994a3c73821c26ebf8cd5d18422d84c879f73
runamonk <runamonk@gmail.com>
2019-11-16 17:40:14 +0000

    Added favorites/collected counts.

--- mnkFavoriteMounts/mnkFavoriteMounts.lua
+++ mnkFavoriteMounts/mnkFavoriteMounts.lua
@@ -4,6 +4,51 @@ mnkFavoriteMounts.LDB = LibStub:GetLibrary('LibDataBroker-1.1')
 local libQTip = LibStub('LibQTip-1.0')
 local libAG = LibStub('AceGUI-3.0')
 
+local tblAll = {}
+local tblCollected = {}
+local tblFavorites = {}
+
+local function parseFavorites()
+	tblFavorites = {}	
+	tblCollected = {}
+	
+	local c = 0
+	local d = 0 
+	
+    if #tblAll > 0 then
+        for i = 1, #tblAll do
+            local mName, spellID, mIcon, active, isUsable, _, isFavorite, _, _, hideOnChar, isCollected, mountID = C_MountJournal.GetMountInfoByID(tblAll[i])
+            
+			if isCollected == true and hideOnChar == false then
+				d = (d + 1)
+				tblCollected[d] = {}
+                tblCollected[d].mName = mName
+                tblCollected[d].mID = mountID
+                tblCollected[d].mIcon = mIcon				
+			end
+			if isFavorite == true and isCollected == true and hideOnChar == false then
+                c = (c + 1)
+                tblFavorites[c] = {}
+                tblFavorites[c].mName = mName
+                tblFavorites[c].mID = mountID
+                tblFavorites[c].mIcon = mIcon
+            end
+        end
+        
+        local sort_func = function(a, b) return a.mName < b.mName end
+        table.sort(tblFavorites, sort_func)
+		table.sort(tblCollected, sort_func)
+    end	
+end
+
+local function getAllMounts()
+	tblAll = {}
+	tblAll = C_MountJournal.GetMountIDs()
+	parseFavorites()
+	mnkFavoriteMounts.LDB.label = 'Favorite Mounts'
+	mnkFavoriteMounts.LDB.text = #tblFavorites .. '/' ..#tblCollected
+end
+
 function mnkFavoriteMounts:DoOnEvent(event)
     if event == 'PLAYER_LOGIN' then
         mnkFavoriteMounts.LDB = LibStub('LibDataBroker-1.1'):NewDataObject('mnkFavoriteMountss', {
@@ -14,9 +59,11 @@ function mnkFavoriteMounts:DoOnEvent(event)
         })
 
         Hotkey, _ = GetBindingKey('RANDOM_MOUNT')
-
+		
         mnkFavoriteMounts.LDB.label = 'Favorite Mounts'
-    end
+    elseif event == 'PLAYER_ENTERING_WORLD' or event == 'COMPANION_LEARNED' then
+		getAllMounts() 
+	end
 end
 
 function mnkFavoriteMounts.DoOnEnter(self)
@@ -26,35 +73,17 @@ function mnkFavoriteMounts.DoOnEnter(self)
     tooltip:SetFont(mnkLibs.DefaultTooltipFont)
     tooltip:SetHeaderFont(mnkLibs.DefaultTooltipFont)
     tooltip:Clear()
-    local tblMountIDs = C_MountJournal.GetMountIDs()
-    local tblMounts = {}
-    local c = 0
 
-    if #tblMountIDs > 0 then
+    if #tblFavorites > 0 then
         tooltip:AddHeader(mnkLibs.Color(COLOR_GOLD)..'Favorites')
-        
-        for i = 1, #tblMountIDs do
-            local mName, spellID, mIcon, active, isUsable, _, isFavorite, _, _, hideOnChar, isCollected, mountID = C_MountJournal.GetMountInfoByID(tblMountIDs[i])
-            
-            if isFavorite == true and isCollected == true and hideOnChar == false then
-                c = (c + 1)
-                tblMounts[c] = {}
-                tblMounts[c].mName = mName
-                tblMounts[c].mID = mountID
-                tblMounts[c].mIcon = mIcon
-            end
-        end
-        
-        local sort_func = function(a, b) return a.mName < b.mName end
-        table.sort(tblMounts, sort_func)
 
-        for i = 1, #tblMounts do
-            local y = tooltip:AddLine(string.format('|T%s:16|t', tblMounts[i].mIcon)..' '..tblMounts[i].mName)
-            tooltip:SetLineScript(y, 'OnMouseDown', mnkFavoriteMounts.DoOnMouseDown, tblMounts[i].mID)
+        for i = 1, #tblFavorites do
+            local y = tooltip:AddLine(string.format('|T%s:16|t', tblFavorites[i].mIcon)..' '..tblFavorites[i].mName)
+            tooltip:SetLineScript(y, 'OnMouseDown', mnkFavoriteMounts.DoOnMouseDown, tblFavorites[i].mID)
         end 
     end
 
-    if (c == 0) then
+    if (#tblFavorites == 0) then
         tooltip:AddLine(mnkLibs.Color(COLOR_GOLD)..'No favorite mounts defined.')
     end
 
@@ -76,4 +105,5 @@ end
 mnkFavoriteMounts:SetScript('OnEvent', mnkFavoriteMounts.DoOnEvent)
 mnkFavoriteMounts:RegisterEvent('PLAYER_LOGIN')
 mnkFavoriteMounts:RegisterEvent('PLAYER_ENTERING_WORLD')
+mnkFavoriteMounts:RegisterEvent('COMPANION_LEARNED')
 
