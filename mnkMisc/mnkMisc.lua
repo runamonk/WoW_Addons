08760dc9d3d71e29a490f8efa16ecc3517c80a76
runamonk <runamonk@gmail.com>
2020-01-02 22:29:32 +0000

    Tidy

--- mnkMisc/mnkMisc.lua
+++ mnkMisc/mnkMisc.lua
@@ -1,39 +1,38 @@
 mnkMisc = CreateFrame('Frame')
+mnkMisc:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
+mnkMisc:RegisterEvent('ADDON_LOADED')
+mnkMisc:RegisterEvent('PLAYER_LOGIN')
 
-function mnkMisc:DoOnEvent(event, arg1, arg2)
-    if event == 'ADDON_LOADED' then
-        if arg1 == 'Blizzard_TalkingHeadUI' then
-            hooksecurefunc('TalkingHeadFrame_PlayCurrent', function()
-                TalkingHeadFrame_CloseImmediately()
-                TalkingHeadFrame:Hide()
-            end)
-        elseif arg1 == 'mnkMisc' then
-            SetCVar('alwaysCompareItems', 0)
-            SetCVar('enableFloatingCombatText', 1)
-            SetCVar('colorChatNamesByClass', 1)
-            SetCVar('chatStyle', 'classic')
-            OrderHall_CheckCommandBar = mnkLibs.donothing
-        end
+function mnkMisc:ADDON_LOADED(event, arg1, arg2)
+    if arg1 == 'Blizzard_TalkingHeadUI' then
+        hooksecurefunc('TalkingHeadFrame_PlayCurrent', function()
+            TalkingHeadFrame_CloseImmediately()
+            TalkingHeadFrame:Hide()
+        end)
+    elseif arg1 == 'mnkMisc' then
+        SetCVar('alwaysCompareItems', 0)
+        SetCVar('enableFloatingCombatText', 1)
+        SetCVar('colorChatNamesByClass', 1)
+        SetCVar('chatStyle', 'classic')
+        OrderHall_CheckCommandBar = mnkLibs.donothing
     end
+end
+
+function mnkMisc:PLAYER_LOGIN()
+    -- Hide dragon heads
+    -- MainMenuBarArtFrame.LeftEndCap:Hide()
+    -- MainMenuBarArtFrame.RightEndCap:Hide()
+
+    --Hide open all mail button, we both hate it.
+    OpenAllMail:Hide()
+
+    BossBanner:Hide()
+    BossBanner.Hide = mnkLibs.donothing()
+    BossBanner.Show = mnkLibs.donothing()
+    BossBanner:UnregisterAllEvents()
 end
 
 SLASH_RL1 = '/rl'
 function SlashCmdList.RL(msg, editbox)
     ReloadUI()
 end
-
--- Hide dragon heads
--- MainMenuBarArtFrame.LeftEndCap:Hide()
--- MainMenuBarArtFrame.RightEndCap:Hide()
-
---Hide open all mail button, we both hate it.
-OpenAllMail:Hide()
-
-BossBanner:Hide()
-BossBanner.Hide = mnkLibs.donothing()
-BossBanner.Show = mnkLibs.donothing()
-BossBanner:UnregisterAllEvents()
-
-mnkMisc:SetScript('OnEvent', mnkMisc.DoOnEvent)
-mnkMisc:RegisterEvent('ADDON_LOADED')
-
