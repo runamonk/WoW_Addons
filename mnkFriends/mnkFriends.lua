99dc8b294ff51f1649b6d5280c166d5c5e8b4384
runamonk <35523636+runamonk@users.noreply.github.com>
2020-01-02 21:00:49 +0000

    Cleanup

--- mnkFriends/mnkFriends.lua
+++ mnkFriends/mnkFriends.lua
@@ -1,38 +1,69 @@
 mnkFriends = CreateFrame('Frame')
 mnkFriends.LDB = LibStub:GetLibrary('LibDataBroker-1.1')
+mnkFriends:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
 
 local LibQTip = LibStub('LibQTip-1.0')
 local FRIENDS_TEXTURE_BROADCAST = 'Interface\\FriendsFrame\\BroadcastIcon'
 local FRIENDS_TEXTURE_ONLINE = 'Interface\\FriendsFrame\\StatusIcon-Online'
-local BNET_ICON = 'Interface\\FriendsFrame\\Battlenet-Portrait'
-local WOW_ICON = 'Interface\\FriendsFrame\\Battlenet-WoWicon'
+
 local LastFriendsOnline = 0
 local colors = {}
 
-function mnkFriends.DoOnMouseDown(self, arg, button) 
-	local sendBNet = false
-	local name = string.sub(arg,3,string.len(arg))
-	
-	if string.sub(arg, 1, 2) ~= 'w_' then
-        sendBNet = true
-	end
-	
-    if button == 'RightButton' then
-        if sendBNet then 
-            return
-        else
-		  C_PartyInfo.InviteUnit(name)
-        end 
-    else
-		if sendBNet then 
-			ChatFrame_SendBNetTell(name)
-		else
-			ChatFrame_SendTell(name)
-		end
-    end
+function mnkFriends:BN_FRIEND_ACCOUNT_OFFLINE()
+    self.LDB.text = mnkFriends.GetNumFriendsOnline()
 end
 
-function mnkFriends.DoOnEnter(self)
+function mnkFriends:BN_FRIEND_ACCOUNT_ONLINE()
+    self.LDB.text = mnkFriends.GetNumFriendsOnline()
+end
+
+function mnkFriends:FRIENDLIST_UPDATE()
+    self.LDB.text = mnkFriends.GetNumFriendsOnline()
+end
+
+function mnkFriends:GetNumFriendsOnline()
+    --C_FriendList.ShowFriends()
+	local i =  C_FriendList.GetNumOnlineFriends()
+	local numBNetTotal, numBNetOnline = BNGetNumFriends()
+
+    if LastFriendsOnline ~= (i + numBNetOnline) then
+        if ((i + numBNetOnline) > LastFriendsOnline) then
+            PlaySoundFile(mnkLibs.Sounds.friend_online, 'Master')
+        end
+        LastFriendsOnline = (i + numBNetOnline)
+    end
+
+    return i + numBNetOnline
+end
+
+function mnkFriends:OnClick()
+    ToggleFriendsFrame(1)
+end
+
+function mnkFriends:OnEnter(parent)
+    local function OnMouseDown(self, arg, button) 
+        local sendBNet = false
+        local name = string.sub(arg,3,string.len(arg))
+        
+        if string.sub(arg, 1, 2) ~= 'w_' then
+            sendBNet = true
+        end
+        
+        if button == 'RightButton' then
+            if sendBNet then 
+                return
+            else
+              C_PartyInfo.InviteUnit(name)
+            end 
+        else
+            if sendBNet then 
+                ChatFrame_SendBNetTell(name)
+            else
+                ChatFrame_SendTell(name)
+            end
+        end
+    end
+
     local tooltip = LibQTip:Acquire('mnkFriendsTooltip', 5, 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT')
     local status = ""
     local t = {}
@@ -105,10 +136,10 @@ function mnkFriends.DoOnEnter(self)
            -- print(t[i].name, ' ', t[i].zone)
             if t[i].client == 'WoW' then
                 local y, x = tooltip:AddLine(t[i].nameformatted..status, t[i].level, t[i].zone, t[i].note)
-                tooltip:SetLineScript(y, 'OnMouseDown', mnkFriends.DoOnMouseDown, 'w_'..t[i].name)
+                tooltip:SetLineScript(y, 'OnMouseDown', OnMouseDown, 'w_'..t[i].name)
             else
                 local y, x = tooltip:AddLine(t[i].name..status, '', t[i].zone, t[i].note)
-                tooltip:SetLineScript(y, 'OnMouseDown', mnkFriends.DoOnMouseDown, 'b_'..t[i].name)      
+                tooltip:SetLineScript(y, 'OnMouseDown', OnMouseDown, 'b_'..t[i].name)      
             end 
         end
 
@@ -121,51 +152,28 @@ function mnkFriends.DoOnEnter(self)
     local l = tooltip:AddLine()
     tooltip:SetCell(l, 1, 'Left click to send a whisper, right click to invite to group.', 5)
 
-    tooltip:SetAutoHideDelay(.1, self)
-    tooltip:SmartAnchorTo(self)
+    tooltip:SetAutoHideDelay(.1, parent)
+    tooltip:SmartAnchorTo(parent)
     tooltip:UpdateScrolling(500)
     tooltip.step = 50
     tooltip:SetBackdropBorderColor(0, 0, 0, 0)
     tooltip:Show()
 end
 
-function mnkFriends.DoOnClick(self)
-    ToggleFriendsFrame(1)
-end
+function mnkFriends:PLAYER_LOGIN()
+    mnkFriends.LDB = LibStub('LibDataBroker-1.1'):NewDataObject('mnkFriends', {
+        icon = 'Interface\\Icons\\Inv_drink_05.blp', 
+        type = 'data source', 
+        OnEnter = function (parent) mnkFriends:OnEnter(parent) end, 
+        OnClick = function () mnkFriends:OnClick() end
+    })
+    for class, color in pairs(RAID_CLASS_COLORS) do colors[class] = string.format('%02x%02x%02x', color.r * 255, color.g * 255, color.b * 255) end
 
-function mnkFriends.GetNumFriendsOnline()
-    --C_FriendList.ShowFriends()
-	local i =  C_FriendList.GetNumOnlineFriends()
-	local numBNetTotal, numBNetOnline = BNGetNumFriends()
-
-    if LastFriendsOnline ~= (i + numBNetOnline) then
-        if ((i + numBNetOnline) > LastFriendsOnline) then
-            PlaySoundFile(mnkLibs.Sounds.friend_online, 'Master')
-        end
-        LastFriendsOnline = (i + numBNetOnline)
-    end
-
-    return i + numBNetOnline
-end
-
-function mnkFriends:DoOnEvent(event)
-    if event == 'PLAYER_LOGIN' then
-        mnkFriends.LDB = LibStub('LibDataBroker-1.1'):NewDataObject('mnkFriends', {
-            icon = 'Interface\\Icons\\Inv_drink_05.blp', 
-            type = 'data source', 
-            OnEnter = mnkFriends.DoOnEnter, 
-            OnClick = mnkFriends.DoOnClick
-        })
-        for class, color in pairs(RAID_CLASS_COLORS) do colors[class] = string.format('%02x%02x%02x', color.r * 255, color.g * 255, color.b * 255) end
-    end
     self.LDB.label = 'Friends'
     self.LDB.text = mnkFriends.GetNumFriendsOnline()
 end
 
-mnkFriends:SetScript('OnEvent', mnkFriends.DoOnEvent)
 mnkFriends:RegisterEvent('PLAYER_LOGIN')
 mnkFriends:RegisterEvent('FRIENDLIST_UPDATE')
---mnkFriends:RegisterEvent('PLAYER_FLAGS_CHANGED')
 mnkFriends:RegisterEvent('BN_FRIEND_ACCOUNT_OFFLINE')
 mnkFriends:RegisterEvent('BN_FRIEND_ACCOUNT_ONLINE')
---mnkFriends:RegisterEvent('BN_FRIEND_INFO_CHANGED')
