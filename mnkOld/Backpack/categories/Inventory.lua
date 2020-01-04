549c1019367993bc9c7d8cec5947d18d6ebe05de
runamonk <35523636+runamonk@users.noreply.github.com>
2020-01-04 10:08:11 +0000

    Deprecated

--- mnkOld/Backpack/categories/Inventory.lua
+++
@@ -1,9 +0,0 @@
-local P, E, L = unpack(select(2, ...))
-
-local categoryName = L['Inventory']
-
-local categoryFilter = function(bagID, slotID)
-	return true -- default path
-end
-
-P.AddCategory(1, categoryName, 'Inventory', categoryFilter)
