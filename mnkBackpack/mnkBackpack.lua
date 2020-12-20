d49eb230930a71434eb7356346f94a050bc4b33e
runamonk <35523636+runamonk@users.noreply.github.com>
2020-12-20 14:59:06 +0000

    Creating basic addon layout. Does nothing yet.

--- mnkBackpack/mnkBackpack.lua
+++ mnkBackpack/mnkBackpack.lua
@@ -1,14 +1,12 @@
-
-local mbBags = {}
+local mnkBackpack = CreateFrame('Frame', 'mnkBackpack', UIParent, BackdropTemplateMixin and "BackdropTemplate")
+local Bags = {}
 local bagMain, bagBank, bagReagent, bagJunk, bagNew = nil
-
-local mnkBackpack = CreateFrame('Frame', 'mnkBags', UIParent, BackdropTemplateMixin and "BackdropTemplate")
-
 local _
+local doInit = true
 local JunkItemsSold = 0
 local NewItemsSold = 0
 
-local mediaPath = [[Interface\AddOns\mnkBags\media\]]
+local mediaPath = [[Interface\AddOns\mnkBackpack\media\]]
 local Textures = {
 	Search =	mediaPath .. "Search",
 	BagToggle =	mediaPath .. "BagToggle",
@@ -18,34 +16,49 @@ local Textures = {
 
 mnkBackpack:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
 mnkBackpack:RegisterEvent('PLAYER_ENTERING_WORLD')
+
 mnkBackpackKnownItems = {}
 
-function mnkBags:PLAYER_ENTERING_WORLD(event, firstTime, reload)
-	if firstTime then
+function mnkBackpack:CreateBag(bagParent, bagName)
+	--print(bagParent, bagName)
+	local bag = CreateFrame("Frame", (bagParent or "")..bagName, self)
+	bag.Caption = bagName
+
+	table.insert(Bags, bag)
+	return bag
+end
+
+function mnkBackpack:Toggle()
+
+end
+
+function mnkBackpack:PLAYER_ENTERING_WORLD(event, firstTime, reload)
+	if doInit or firstTime then
+		ToggleBackpack = function(self) mnkBackpack:Toggle() end
+		OpenAllBags = function(self) mnkBackpack:Toggle() end
+		OpenBackpack = function(self) mnkBackpack:Toggle() end
+		CloseAllBags = function(self) mnkBackpack:Toggle() end
+		CloseBackpack = function(self) mnkBackpack:Toggle() end
+		ToggleAllBags = function(self) mnkBackpack:Toggle() end
+
 		for i = 0, NUM_LE_ITEM_CLASSS-1 do
-			CreateBag("_bag", GetItemClassInfo(i))
-			CreateBag("_bank", GetItemClassInfo(i))
+			self:CreateBag("_bag", GetItemClassInfo(i))
+			self:CreateBag("_bank", GetItemClassInfo(i))
 		end
 
-		bagJunk = CreateBag("_bag","Junk")
-		bagNew = CreateBag("_bag", "New")
+		bagJunk = self:CreateBag("_bag","Junk")
+		bagNew = self:CreateBag("_bag", "New")
 
-		local sort_func = function(a, b) return a.name > b.name end
-	    table.sort(mbBags, sort_func)
+		local sort_func = function(a, b) return a:GetName() > b:GetName() end
+	    table.sort(Bags, sort_func)
 
-		bagReagent = CreateBag("_bank","Reagents")
-		bagBank = CreateBag(nil,"_bank")
-		bagMain = CreateBag(nil,"_bag")
+		bagReagent = self:CreateBag("_bank","Reagents")
+		bagBank = self:CreateBag(nil,"_bank")
+		bagMain = self:CreateBag(nil,"_bag")
+
 		bagMain:SetPoint('BOTTOMRIGHT', -20, 220)
 		bagBank:SetPoint('BOTTOMLEFT', 20, 220)
 	end
-end
-
-function mnkBackpack:CreateBag(bagParent, bagClass)
-	local bag = CreateFrame("Frame", bagParent..bagClass, self)
-
-	table.insert(mbBags, bag)
-	return bag
 end
 
 
